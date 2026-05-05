88e0d85197efdf03e44d4979f02c4c8af56123e8
epetela51 <57976052+epetela51@users.noreply.github.com>
2026-05-04 20:08:29 -0400

    Update SelectionPanel.jsx

--- src/pages/BreakerFinder/SelectionPanel/SelectionPanel.jsx
+++ src/pages/BreakerFinder/SelectionPanel/SelectionPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import FloorSelector from './Selectors/FloorSelector/FloorSelector';
 import RoomSelector from './Selectors/RoomSelector/RoomSelector';
 import TypeSelector from './Selectors/TypeSelector/TypeSelector';
